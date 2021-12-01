bf80ce7fd828b9624d3bf2a3cd70d25cf1bcabac
Emerson Cloud <emersoncloud@Emersons-Lair.local>
2021-11-30 19:36:17 -0500

    have i even committed this yet

---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+use std::fmt;
+use web_sys;
+use js_sys;
+use fixedbitset::FixedBitSet;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Universe {
+        let width = 2;
+        let height = 2;
+
+        let size = (width * height) as usize;
+
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+            cells.set(i, true);
+        }
+
+        Universe {
+            width,
+            height, 
+            cells,
+        }
+    }
+
+    pub fn print_cells(&self) {
+        let d = js_sys::Array::new();
+        for x in (0..self.cells.len()) {
+            let y = self.cells[x] as u32;
+            d.push(&JsValue::from(y));
+        }
+        web_sys::console::log(&d);
+    }
+     
+    pub fn new_size(w: u32, h: u32) -> Universe {
+        let size = (w * h) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.5);
+            // cells.set(i, true);
+        }
+
+        Universe {width: w, height: h, cells: cells}
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        // self.print_cells();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                next.set(idx, match (cell, live_neighbors) {
+                    (true, x) if x < 2 => false,
+                    (true, 2) | (true, 3) => true,
+                    (true, x) if x > 3 => false,
+                    (false, 3) => true,
+                    (otherwise, _) => otherwise,
+                });
+            }
+        }
+        self.cells = next;
+    }
+
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+                
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);  
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
